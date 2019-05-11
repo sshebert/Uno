aab0adec5ec7934626054bf45ffdbe5ceb96cbda
Sam Shebert <45662712+sshebert@users.noreply.github.com>
2019-05-11 17:44:04 -0400

    Add files via upload

--- Game/Shared/Constants.java
+++ Game/Shared/Constants.java
@@ -10,7 +10,14 @@ package Game.Shared;
  * @author alawren3
  */
 public class Constants {
-    public final int PORT = 2696;
-    public final int MAXDATASIZE = 1000;
-    public final String server = "pi.cs.oswego.edu";
+    public static final int PORT = 2696;
+    public static final int MAXDATASIZE = 1000;
+    public static final String server = "pi.cs.oswego.edu";
+    public static final int minIndex = 1;
+    public static final int drawCard = minIndex - 1;
+    public static final int play = minIndex - 1;
+    public static final int hold = minIndex - 2;
+    public static final int exit = minIndex - 3;
+    public static final int timeout = minIndex - 4;
+    public static final int startGame = minIndex - 5;
 }
