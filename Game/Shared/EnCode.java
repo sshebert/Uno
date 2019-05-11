cfe1d49b095ff86a0fbfe08193e693bad6835285
Sam Shebert <45662712+sshebert@users.noreply.github.com>
2019-05-11 16:12:40 -0400

    Add files via upload

--- Game/Shared/EnCode.java
+++ Game/Shared/EnCode.java
@@ -23,6 +23,7 @@ public class EnCode {
     /*
     for codes: 0, 2, 3, 4, 8, 9, 10, 12
      */
+    //opcode 3
     public EnCode(int ackOpCode){
         try {
             stream.write(intToBytes(ackOpCode));
@@ -32,10 +33,7 @@ public class EnCode {
         }
     }
 
-    /*
-    for codes: 1
-     */
-
+    //opcode 1
     public EnCode(String ip){
         try {
             stream.write(intToBytes(1));
@@ -46,23 +44,7 @@ public class EnCode {
         }
     }
 
-    /*
-    for codes: 5, 6, 7, 11
-     */
-
-    public EnCode(Card card, int opCode){
-        try {
-            stream.write(intToBytes(opCode));
-            if (card != null) {
-                ObjectOutputStream out = new ObjectOutputStream(stream);
-                out.writeObject(card);
-            }
-        }
-        catch (IOException exp){
-            exp.printStackTrace();
-        }
-    }
-
+    //opcode 2
     public EnCode(ClientGame game, int opCode){
         try {
             stream.write(intToBytes(opCode));
@@ -76,13 +58,13 @@ public class EnCode {
         }
     }
 
-    public EnCode(Boolean skip, int opCode){
+    //opcode 0
+    public EnCode(Player player, int opCode){
         try {
             stream.write(intToBytes(opCode));
-            if(skip){
-                stream.write(1);
-            }else{
-                stream.write(0);
+            if (player != null) {
+                ObjectOutputStream out = new ObjectOutputStream(stream);
+                out.writeObject(player);
             }
         }
         catch (IOException exp){
