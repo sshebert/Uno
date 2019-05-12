3495149805ce5b4f13b3ddb7d84794a32f95f702
Sam Shebert <45662712+sshebert@users.noreply.github.com>
2019-05-11 21:00:26 -0400

    Add files via upload

--- Game/Client/Main.java
+++ Game/Client/Main.java
@@ -1,4 +1,3 @@
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -106,39 +105,50 @@ public class Main {
         ArrayList<Player> tempList = new ArrayList<>();
         tempList.add(me);
         UniCastProtocol lobbyUniCast = new UniCastProtocol();
-        Player newPlayer;
-        Player newPlayer2;
-        boolean gameStart = false;
+        Player newPlayer = null;
+        Player newPlayer2 = null;
+        long time = System.nanoTime();
         int message = Constants.drawCard;
         while (message != Constants.startGame) {
             try {
-                DeCode deCode = new DeCode(lobbyUniCast.recieve(3, 1400));
-                System.out.println("Received Uni Message");
-                newPlayer = deCode.player;
-                if (deCode.opcode == 0 && deCode.player != null) {
+                byte[] temp = lobbyUniCast.recieve(3, 1400);
+                if (temp != null) {
+                    DeCode deCode = new DeCode(temp);
+
                     newPlayer = deCode.player;
-                    System.out.println(newPlayer.getName());
-                    if (tempList.contains(newPlayer) == false) {
-                        EnCode enCode = new EnCode(multicastIP);
-                        lobbyUniCast.send(enCode.getHeader(), lobbyUniCast.getLastReceivedAddress());
-                        System.out.println("Sent Multicast IP");
+                    if (deCode.opcode == 0 && deCode.player != null) {
+                        newPlayer = deCode.player;
+                        System.out.println(newPlayer.getName());
+                        if (tempList.contains(newPlayer) == false) {
+                            EnCode enCode = new EnCode(multicastIP);
+                            lobbyUniCast.send(enCode.getHeader(), lobbyUniCast.getLastReceivedAddress());
+                            System.out.println("Sent Multicast IP");
+                        }
                     }
                 }
 
-                deCode = new DeCode(multiCastProtocol.receive(1400));
-                System.out.println("Received Multicast Message");
-                if (deCode.opcode == 0 && deCode.player != null) {
-                    System.out.println("correct opcode");
-                    newPlayer2 = deCode.player;
-                    if (newPlayer.getInetAddress().equals(newPlayer2.getInetAddress())) {
-                        System.out.println("equals previous player");
-                        tempList.add(newPlayer);
-                        System.out.println("Just Added Player " + newPlayer.getName());
+                temp = multiCastProtocol.receive(1400);
+                if (temp != null) {
+                    DeCode deCode = new DeCode(multiCastProtocol.receive(1400));
+                    System.out.println("Received Multicast Message");
+                    if (deCode.opcode == 0 && deCode.player != null) {
+                        System.out.println("correct opcode");
+                        newPlayer2 = deCode.player;
+                        if (newPlayer.getInetAddress().equals(newPlayer2.getInetAddress())) {
+                            System.out.println("equals previous player");
+                            tempList.add(newPlayer);
+                            System.out.println("Just Added Player " + newPlayer.getName());
+                        }
                     }
+                }
+                if (System.nanoTime() - time > (Constants.timeoutNanos / 6L)) {
+                    time = System.nanoTime();
+                    multiCastProtocol.send((new EnCode(3)).getHeader());
+                    System.out.println("Just sent keep alive");
                 }
 
             } catch (InterruptedIOException exp) {
-                exp.printStackTrace();
+                //exp.printStackTrace();
             }
 
             if (messages.size() > 0) {
@@ -281,48 +291,4 @@ public class Main {
         return null;
     }
 
-}
-
-class ClientListener implements Runnable {
-
-    String helpResponse = "Commands:\n\t\"current player\" - returns name of current player\n\t";
-
-    @Override
-    public void run() {/*
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Hey, I'm Robo Rob!");
-        System.out.println("The main method is busy right now, so I'll be processing your requests :)");
-        while (true) {
-            String message = sc.nextLine();
-            switch (message) {
-                case "help":
-                    System.out.println(helpResponse);
-                    break;
-                case "current player":
-                    System.out.println(ClientGame.getCurrPlayer().getName());
-                    break;
-                case "current player hand size":
-                    System.out.println(ClientGame.getCurrPlayerHandSize());
-                    break;
-                case "my cards":
-                    ClientGame.getMyPlayer().printCards();
-                    break;
-                case "my hand size":
-                    System.out.println(ClientGame.getMyPlayer().getHandLength());
-                    break;
-                case "top card":
-                    System.out.println(ClientGame.getTopCard().toString());
-                    break;
-                case "play card":
-                    ClientGame.getMyPlayer().printCards();
-                    System.out.println("Enter the index of the card you would like to play");
-                    String cardIndex = sc.nextLine();
-                    Main.messages.add(cardIndex);
-                    break;
-                default:
-                    System.out.println("Sorry I don't know what your talking about, type \"help\" for the list of commands");
-                    break;
-            }
-        }*/
-    }
 }
