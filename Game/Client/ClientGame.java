b322884846a8b81daeb4d0d7fd647bc771b47d81
Sam Shebert <45662712+sshebert@users.noreply.github.com>
2019-05-15 10:40:58 -0400

    updated with comments/author

--- Game/Client/ClientGame.java
+++ Game/Client/ClientGame.java
@@ -1,3 +1,5 @@
+//Author Sam Shebert
+
 package Game.Client;
 
 import Game.Shared.*;
@@ -22,7 +24,7 @@ public class ClientGame implements Serializable{
         deck = new Deck(multiple);
 
         Player first = currPlayer = players.current();
-        for (int i = 0; i < startingHandSize; i++) {
+        for (int i = 0; i < startingHandSize; i++) {//deal cards
             first.drawCard(deck.drawCard());
             while (players.next() != first)
                 players.current().drawCard(deck.drawCard());
@@ -33,7 +35,7 @@ public class ClientGame implements Serializable{
 
     public boolean checkCurrPlayer() {
         try {
-            if (currPlayer.getInetAddress().equals(InetAddress.getLocalHost())) {
+            if (currPlayer.getInetAddress().equals(InetAddress.getLocalHost())) {//check player based on IP address
                 return true;
             } else {
                 return false;
@@ -63,7 +65,7 @@ public class ClientGame implements Serializable{
         return skip;
     }
 
-    public Card[] getSkipCards() {
+    public Card[] getSkipCards() {//handles skip cards, removes from deck and assigns to player
         Card[] cards = null;
         if (drawTwo) {
             cards = new Card[2];
@@ -88,19 +90,19 @@ public class ClientGame implements Serializable{
     }
 
     public boolean callUno(Player player){
-        if(player.equals(currPlayer)){
-            if(currPlayer.getHandLength() <= 2){
+        if(player.equals(currPlayer)){//calling uno for yourself
+            if(currPlayer.getHandLength() <= 2){//must have 2 cards to call uno on yourself
                 currPlayer.setUnoSafe(true);
                 return true;
-            }else{
+            }else{//if you have more than 2 cards then draw cards
                 drawCard();
                 drawCard();
                 currPlayer.setUnoSafe(false);
                 return false;
             }
-        }else{
+        }else{//calling uno on someone else
             boolean b = false;
-            for(int i = 0; i < players.size(); i++){
+            for(int i = 0; i < players.size(); i++){//check that someone has 1 cards
                 players.next();
                 if(players.current().getHandLength() == 1){
                     players.current().drawCard(deck.drawCard());
@@ -108,7 +110,7 @@ public class ClientGame implements Serializable{
                     b = true;
                 }
             }
-            if(!b){
+            if(!b){//if no one has 1 card, then you draw card
                 players.seek(player);
                 players.current().drawCard(deck.drawCard());
                 players.current().drawCard(deck.drawCard());
@@ -118,7 +120,7 @@ public class ClientGame implements Serializable{
         }
     }
 
-    public boolean resolveUno(InetAddress playerInet){
+    public boolean resolveUno(InetAddress playerInet){//get player who called uno based on ip
         Player out = null;
         for(int i = 0; i < players.size(); i++){
             players.next();
@@ -144,7 +146,7 @@ public class ClientGame implements Serializable{
         return false;
     }
 
-    public boolean playDrawCard(PlayerMove playerMove) {
+    public boolean playDrawCard(PlayerMove playerMove) {//case if someone drew a card and is trying to play it
         if (playerMove.card.equals(currPlayer.getLastDrewCard())) {
             if (Card.checkMove(deck.getTopCard(), playerMove.card)) {
                 resolveMove(playerMove);
@@ -154,7 +156,7 @@ public class ClientGame implements Serializable{
         return false;
     }
 
-    public Card getCard(int index){
+    public Card getCard(int index){//method to play card
         return currPlayer.getCard(index);
     }
 
@@ -164,17 +166,17 @@ public class ClientGame implements Serializable{
         return drawCard;
     }
 
-    private void resolveWild(Suit wildSuit) {
+    private void resolveWild(Suit wildSuit) {//when wild is a played, another temporary card is put on top with the color of the wild
         Card blankWild = new Card(CardType.Wild, wildSuit);
         deck.addWild(blankWild);
     }
 
-    private void resolveWildDraw(Suit wildSuit) {
+    private void resolveWildDraw(Suit wildSuit) {//same as wild but for drawing
         Card blankWild = new Card(CardType.WildDraw, wildSuit);
         deck.addWild(blankWild);
     }
 
-    private void resolveMove(PlayerMove playerMove) {
+    private void resolveMove(PlayerMove playerMove) {//meant for resolving special cards, sets internal logic for handling special cards
         deck.playCard(playerMove.card);
         currPlayer.playCard(playerMove.card);
         //handle action cards
@@ -217,161 +219,4 @@ public class ClientGame implements Serializable{
     public Player getWinner(){
         return winner;
     }
-
-
-   /* public void run() {
-        Scanner sc = new Scanner(System.in);
-        //deal cards
-
-
-        while (true) {
-            Player p = players.next();
-            //skip will always be true if drawTwo or drawFour is true, so only checking skip for now
-            if (!skip) {
-                System.out.println("\n" + p.getName() + "'s turn");
-                System.out.println("Top card is " + deck.getTopCard().toString() + "\n");
-                System.out.println("Your cards are:");
-                p.printCards();
-
-                boolean moveValid = false;
-                while (!moveValid) {
-                    System.out.println("Enter the index of the card you wish to play or type draw");
-                    String move = sc.nextLine();
-
-                    if (move.equalsIgnoreCase("draw")) {
-                        Card drawCard = deck.drawCard();
-                        p.drawCard(drawCard);
-                        System.out.println(drawCard.toString() + " drawn");
-                        System.out.println("Type yes to play card draw");
-                        move = sc.nextLine();
-                        if (move.equalsIgnoreCase("yes")) {
-                            if (ClientGame.checkMove(deck.getTopCard(), drawCard)) {
-                                p.playCard(drawCard);
-                                deck.playCard(drawCard);
-                            }
-                        }
-                        moveValid = true;
-                    } else if (isNumeric(move)) {
-                        int cardNum = Integer.parseInt(move);
-                        Card playCard = p.getCard(cardNum);
-
-                        if (playCard.getSuit() == Suit.Wild) {
-                            p.playCard(playCard);//remove from player hand
-                            deck.playCard(playCard);//add to deck
-                            //create temporary card based on desired suit and add to deck using special deck.addWild() method
-                            //the deck will handle removing the blank card when the next card is played.
-                            Card blankWild = null;
-                            while (blankWild == null) {
-                                System.out.println("What color would you like to change to?");
-                                String newColor = sc.nextLine();
-                                switch (newColor) {
-                                    case "green":
-                                        blankWild = new Card(CardType.Wild, Suit.Green);
-                                        break;
-                                    case "red":
-                                        blankWild = new Card(CardType.Wild, Suit.Red);
-                                        break;
-                                    case "blue":
-                                        blankWild = new Card(CardType.Wild, Suit.Blue);
-                                        break;
-                                    case "yellow":
-                                        blankWild = new Card(CardType.Wild, Suit.Yellow);
-                                        break;
-                                    default:
-                                        System.out.println("Invalid suit, pick green, red, blue, or yellow");
-                                        break;
-                                }
-                            }
-                            deck.addWild(blankWild);
-
-                            if (playCard.getCardVal() == CardType.WildDraw) {
-                                skip = true;
-                                drawFour = true;
-                            }
-                            moveValid = true;
-
-                        } else if (ClientGame.checkMove(deck.getTopCard(), playCard)) {
-                            p.playCard(playCard);
-                            deck.playCard(playCard);
-                            switch (playCard.getCardVal()) {
-                                case Reverse:
-                                    players.flipDirection();
-                                    break;
-                                case DrawTwo:
-                                    drawTwo = true;
-                                    skip = true;
-                                    break;
-                                case Skip:
-                                    skip = true;
-                                    break;
-                                default:
-                                    break;
-                            }
-                            moveValid = true;
-                        } else {
-                            System.out.println("Move not valid, try again.");
-                        }
-
-                    } else if (move.equalsIgnoreCase("end")) {
-                        moveValid = true;
-                    } else {
-                        System.out.println("Command not valid, try again.");
-                    }
-
-                    if (p.getHandLength() == 0) {
-                        System.out.println(p.getName() + " won");
-                        return;
-                    }
-                }
-                //handle skip and stuff
-            } else {
-                System.out.println("\n" + p.getName() + "'s turn was skipped");
-                if (drawTwo) {
-                    Card temp = deck.drawCard();
-                    System.out.println(p.getName() + " drew " + temp.toString());
-                    p.drawCard(temp);
-
-                    temp = deck.drawCard();
-                    System.out.println(p.getName() + " drew " + temp.toString());
-                    p.drawCard(temp);
-                    drawTwo = false;
-                } else if (drawFour) {
-                    Card temp = deck.drawCard();
-                    System.out.println(p.getName() + " drew " + temp.toString());
-                    p.drawCard(temp);
-
-                    temp = deck.drawCard();
-                    System.out.println(p.getName() + " drew " + temp.toString());
-                    p.drawCard(temp);
-
-                    temp = deck.drawCard();
-                    System.out.println(p.getName() + " drew " + temp.toString());
-                    p.drawCard(temp);
-
-                    temp = deck.drawCard();
-                    System.out.println(p.getName() + " drew " + temp.toString());
-                    p.drawCard(temp);
-                    drawFour = false;
-                }
-                skip = false;
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number of players");
-        int playersNum = sc.nextInt();
-        sc.nextLine();
-        Player p = new Player("tempRemove");
-        CyclicLinkedList<Player> tempPlayers = new CyclicLinkedList<Player>(p);
-        for (int i = 1; i <= playersNum; i++) {
-            System.out.println("Enter player " + i + "'s name");
-            tempPlayers.add(new Player(sc.nextLine()));
-        }
-        tempPlayers.remove(p);
-        ClientGame game = new ClientGame(tempPlayers, 1);
-        game.run();
-    }*/
-
 }
