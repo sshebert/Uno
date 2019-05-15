1f26cd047764e1d17e81994abc606030d82b2e7b
Sam Shebert <45662712+sshebert@users.noreply.github.com>
2019-05-15 10:47:01 -0400

    updated comments

--- Game/Shared/PlayerMove.java
+++ Game/Shared/PlayerMove.java
@@ -1,9 +1,13 @@
+//author Sam Shebert
+//strictly used for communicating between UI(in Main) and ClientGame
+//simplifies communication by putting all possible user decisions into one object and passing it to ClientGame class to resolve the players move.
+
 package Game.Shared;
 
 public class PlayerMove {
-    public Card card;//card player wants to play, can be null which would signal player wants to draw a card
+    public Card card;//card player wants to play
     public Suit wildSuit;//if player is playing a wild card they need to include which suit they are changing too, else this will be null
-    public boolean draw;
+    public boolean draw;//signals player wants to draw card
 
     public PlayerMove(Card card) {
         this.card = card;
